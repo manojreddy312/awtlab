3d0770608284e0dc141393418c528ebc73984a1b
manojreddy312 <101630894+manojreddy312@users.noreply.github.com>
2022-03-15 16:52:35 +0530

    First commit

---
+++ calc.js
@@ -0,0 +1,4 @@
+import {area,fun} from "./demo.js"
+var a=2,b=4
+console.log(area(a,b))
+console.log(fun())
---
+++ demo.js
@@ -0,0 +1,8 @@
+export function area(a,b){
+    return a*b
+}
+function fun(){
+    return 10
+}
+
+export {fun}
