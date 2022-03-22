593573d6f38bf5b309da8b0602d558debe38b292
manojreddy312 <101630894+manojreddy312@users.noreply.github.com>
2022-03-22 15:02:36 +0530

    Add files via upload

---
+++ movie.js
@@ -0,0 +1,10 @@
+function getReview(){
+    var request = new XMLHttpRequest();
+    var url = 'https://imdb-api.com/en/API/UserRatings/k_io03whlz/tt1375666';
+    request.open('GET',url,true);
+    request.onload=function(){
+        var result=JSON.parse(this.response);
+        console.log(result);
+    }
+    request.send();
+}
